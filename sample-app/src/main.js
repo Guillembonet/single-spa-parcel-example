240cb491ecab66b2adc0768775aff4094b708d47
Guillem Bonet <guillem.bonet-arnaiz@sogeti.com>
2019-10-21 13:11:49 +0200

    single spa test first commit

--- sample-app/src/main.js
+++ sample-app/src/main.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import singleSpaVue from 'single-spa-vue';
+import './set-public-path'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+const vueLifecycles = singleSpaVue({
+  Vue,
+  appOptions: {
+    render: (h) => h(App),
+  },
+});
+
+export const bootstrap = vueLifecycles.bootstrap;
+export const mount = vueLifecycles.mount;
+export const unmount = vueLifecycles.unmount;
---
+++ sample-app/vue.config.js
@@ -0,0 +1,9 @@
+// Temporary until we can use https://github.com/webpack/webpack-dev-server/pull/2143
+module.exports = {
+    chainWebpack: config => {
+        config.devServer.set('inline', false)
+        config.devServer.set('hot', false)
+        config.externals(['vue', 'vue-router'])
+    },
+    filenameHashing: false,
+}
