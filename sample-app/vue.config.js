f82520491758b42e3773fa270122bec6e84b5e83
Guillem Bonet <guillem.bonet-arnaiz@sogeti.com>
2019-10-21 13:24:54 +0200

    no shared dependency

--- sample-app/vue.config.js
+++ sample-app/vue.config.js
@@ -3,7 +3,6 @@ module.exports = {
     chainWebpack: config => {
         config.devServer.set('inline', false)
         config.devServer.set('hot', false)
-        config.externals(['vue', 'vue-router'])
     },
     filenameHashing: false,
 }
