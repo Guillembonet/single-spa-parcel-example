01a9e882b2803979e9e3210465e2c04a1a46ebd7
Guillem <guillem.bonet@gmail.com>
2019-11-20 16:55:41 +0100

    shared vue and react dependencies

--- sample-react/webpack.config.js
+++ sample-react/webpack.config.js
@@ -47,7 +47,7 @@ module.exports = () => {
                 "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
                 "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
             }
-        }
-        
+        },
+        externals: ['react', 'react-dom']
     }
 };
--- sample-vue-parcel/vue.config.js
+++ sample-vue-parcel/vue.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     chainWebpack: config => {
         config.devServer.set('inline', false)
-        config.devServer.set('hot', false)
+        config.devServer.set('hot', false),
+        config.externals(['vue'])
     },
     filenameHashing: false
 }
